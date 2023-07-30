ce5f5227d1879e30250ab5bc03ac1c0824ca5e17
siripods <siripods@gmail.com>
2023-07-30 23:13:53 +0700

    Add hero page

---
+++ components/home-page/hero.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image src="/images/site/max.png" alt="" width={300} height={300} />
+      </div>
+      <h1>Hi, I'm Max</h1>
+      <p>I blog about web development</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+}
+
+export default HomePage;
+
+// 1) Hero
+// 2) Featured products
