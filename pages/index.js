6aba1f13ae9a80059ba54bdf4e8da288dde4a4fd
siripods <siripods@gmail.com>
2023-07-31 12:18:23 +0700

    Add Featured Posts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  { slug: 'getting-started-with-nextjs', 
+  title: 'Getting started with NextJS', 
+  image: 'getting-started-nextjs.png', 
+  excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR', 
+  date: '2022-02-10'},
+  { slug: 'getting-started-with-nextjs2', 
+  title: 'Getting started with NextJS', 
+  image: 'getting-started-nextjs.png', 
+  excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR', 
+  date: '2022-02-10'},
+  { slug: 'getting-started-with-nextjs3', 
+  title: 'Getting started with NextJS', 
+  image: 'getting-started-nextjs.png', 
+  excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR', 
+  date: '2022-02-10'},
+  { slug: 'getting-started-with-nextjs4', 
+  title: 'Getting started with NextJS', 
+  image: 'getting-started-nextjs.png', 
+  excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR', 
+  date: '2022-02-10'}
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS}/>
     </Fragment>
   );
 }
