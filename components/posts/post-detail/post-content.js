a884953434bad024c247dc2c49447daffe9a66d1
siripods <siripods@gmail.com>
2023-07-31 23:37:18 +0700

    Use custom renderer with Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,23 +2,56 @@ import React from "react";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Image from "next/image";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
   title: "Getting started with NextJS",
   image: "getting-started-nextjs.png",
   date: "2022-02-10",
-  content: "# This is a first post"
+  content: "# This is a first post",
 };
 
 function PostContent(props) {
-  const {post} = props;
+  const { post } = props;
 
-    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  //customize renderes so that it renders JSX image the way we want, instead of render standard image tag
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
